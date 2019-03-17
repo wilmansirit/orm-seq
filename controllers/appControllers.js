229a95e4351758b5b25e53dfe92b29e88653f42b
wilmansirit <wsirit@gmail.com>
2019-03-16 20:38:53 -0400

    Creando el primer modelo

--- controllers/appControllers.js
+++ controllers/appControllers.js
@@ -1,14 +1,18 @@
 'use strict'
 
+const 	User 		= require('../models/Users')							
+
+
 module.exports = {
 
 
 	index	:	(req, res, next) => {
 
-					res.render('index', {title: 'Ideal Server'})
-
-				}
-
-
-
+				User.findAll()
+					.catch(console.error)
+					.then(data => {
+						res.render('index', {users: JSON.stringify(data, null, 4)})
+					})
+				},
 }
+
--- database/index.js
+++ database/index.js
@@ -20,11 +20,14 @@ const sequelize = new Sequelize(
 	}
 );
 
-sequelize
-	.authenticate()
-	.then(() => {
-		console.log('Connection has been established successfully.');
-	})
-	.catch(err => {
-		console.error('Unable to connect to the database:', err);
-	});
+
+module.exports = sequelize;
+
+// sequelize
+// 	.authenticate()
+// 	.then(() => {
+// 		console.log('Connection has been established successfully.');
+// 	})
+// 	.catch(err => {
+// 		console.error('Unable to connect to the database:', err);
+// 	});
